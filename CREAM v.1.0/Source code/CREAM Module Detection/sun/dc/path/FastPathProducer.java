eeebdbac18162e436dcf4d6968b917ada0ae0afd
Mingfei Han <free1234hm@163.com>
2024-01-17 12:59:51 +0800

    Delete CREAM v.1.0/Source code directory

--- CREAM v.1.0/Source code/CREAM Module Detection/sun/dc/path/FastPathProducer.java
+++
@@ -1,8 +0,0 @@
-package sun.dc.path;
- 
- public interface FastPathProducer {
- 
-    public void getBox(float[] box)     throws PathError;
- 
-   public void sendTo(PathConsumer pc) throws PathError, PathException;
- }
